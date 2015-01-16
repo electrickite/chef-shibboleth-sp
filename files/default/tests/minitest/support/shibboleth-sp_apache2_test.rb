4f534de4762f166b52452fdd2bd0ef9563af5b47
Corey Hinshaw <hinshaw.25@osu.edu>
2015-01-16 16:44:21 -0500

    Remove apache2 test

--- files/default/tests/minitest/support/shibboleth-sp_apache2_test.rb
+++
@@ -1,7 +0,0 @@
-require "minitest/autorun"
-
-describe_recipe "shibboleth-sp::apache2" do
-  include MiniTest::Chef::Assertions
-  include MiniTest::Chef::Context
-  include MiniTest::Chef::Resources
-end
